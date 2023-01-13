5ef7501d79f4a6a801912c5f70b54296504b6e27
tobidosumu <60399800+tobidosumu@users.noreply.github.com>
2023-01-13 23:33:26 +0100

    using hooks

--- App.tsx
+++ App.tsx
@@ -3,94 +3,21 @@ import { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  return <div>{isLoggedIn ? <ProfileCard /> : <Card />}</div>;
-}
-
-{
-  /* Card component starts from here */
-}
-
-let isLoggedIn = true;
-
-// const user = ['Tobi', 'Shad'];
-const users = {
-  fName: 'Tobi',
-  lName: 'Dosumu',
-};
-
-const Card = () => {
-  return (
-    <div className="mainTxt" style={{ marginBottom: '30px' }}>
-      <h1 style={{ color: 'blue' }}>Welcome to my app {users.fName}</h1>
-      <MyButton />
-    </div>
-  );
-};
-
-const MyButton = () => {
-  return <button>I'm a button</button>;
-};
-
-{
-  /* ProfileCard component starts from here */
-}
-
-const user = {
-  name: 'Tobi Dosumu',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
-
-const ProfileCard = () => {
-  return (
-    <div className="mainTxt">
-      <h2> {user.name} </h2>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        // alt={'Photo of ' + user.name}
-        alt={`Photo of ${user.name}`}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-      <MyPets />
-      <MyAlertBtn />
-      <MyAlertBtn />
-
-    </div>
-  );
-};
-
-const pets = [
-  { name: 'Perry', myPet: true, id: 1 },
-  { name: 'Panda', myPet: true, id: 2 },
-  { name: 'Kim', myPet: true, id: 3 },
-  { name: 'Captain', myPet: false, id: 4 },
-];
-
-const MyPets = () => {
-  const petsList = pets.map((pet) => (
-    <li key={pet.id} style={{ color: pet.myPet ? 'blue' : 'black' }}>
-      {pet.name}
-    </li>
-  ));
-
-  return <ul>{petsList}</ul>;
-};
-
-function MyAlertBtn() {
   const [count, setCount] = useState(0);
 
-  function alertBtn() {
-    // alert('You clicked me!')
+  function handleCount() {
     setCount(count + 1);
   }
 
   return (
-    <button onClick={alertBtn} className="alertBtn">
-      Alert button clicked {count} times
-    </button>
+    <div>
+      <h3>Click counter</h3>
+      <MyButton count={count} onClick={handleCount} />
+      <MyButton count={count} onClick={handleCount} />
+    </div>
   );
+}
+
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
 }

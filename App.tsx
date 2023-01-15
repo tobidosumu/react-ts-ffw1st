84b0fea909bd518207ea961deb8fd5d8a847798e
tobidosumu <60399800+tobidosumu@users.noreply.github.com>
2023-01-16 00:08:25 +0100

    add tic-tac-to board

--- App.tsx
+++ App.tsx
@@ -2,22 +2,43 @@ import * as React from 'react';
 import { useState } from 'react';
 import './style.css';
 
-export default function App() {
-  const [count, setCount] = useState(0);
-
-  function handleCount() {
-    setCount(count + 1);
-  }
+export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
 
   return (
     <div>
-      <h3>Click counter</h3>
-      <MyButton count={count} onClick={handleCount} />
-      <MyButton count={count} onClick={handleCount} />
+      <div className="board-row">
+        <Square value={square[0]} />
+        <Square value={square[1]} />
+        <Square value={square[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={square[3]} />
+        <Square value={square[4]} />
+        <Square value={square[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={square[6]} />
+        <Square value={square[7]} />
+        <Square value={square[8]} />
+      </div>
     </div>
   );
 }
 
-function MyButton({ count, onClick }) {
-  return <button onClick={onClick}>Clicked {count} times</button>;
+function Square({ value }) {
+  // const [value, setValue] = useState(null)
+
+  // function handleClick() {
+  //   setValue('X')
+  // }
+
+  return (
+    <button
+      className="square"
+      // onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }

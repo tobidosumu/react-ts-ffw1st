b254920e80ada18dbb2ea733b1b2c95700231c5a
tobidosumu <60399800+tobidosumu@users.noreply.github.com>
2023-01-19 13:30:42 +0100

    gamer O now plays first while gamer X plays next

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
+  const [xIsNext, setXIsNext] = useState(false)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick(i) {
